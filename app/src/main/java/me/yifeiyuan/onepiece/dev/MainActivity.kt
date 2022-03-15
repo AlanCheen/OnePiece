29b3d120377afd7645eaf8a591ca6db1ad533acd
mingjue <zhening.czn@alibaba-inc.com>
2022-03-15 16:29:31 +0800

    add isNetworkAvailable

--- app/src/main/java/me/yifeiyuan/onepiece/dev/MainActivity.kt
+++ app/src/main/java/me/yifeiyuan/onepiece/dev/MainActivity.kt
@@ -2,10 +2,17 @@ package me.yifeiyuan.onepiece.dev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import me.yifeiyuan.onepiece.pandora.ktx.copyTextToClipboard
+import me.yifeiyuan.onepiece.pandora.ktx.start
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun testContext() {
+        start<MainActivity>()
     }
 }
--- pandora/src/main/java/me/yifeiyuan/onepiece/pandora/ktx/Contextx.kt
+++ pandora/src/main/java/me/yifeiyuan/onepiece/pandora/ktx/Contextx.kt
@@ -1,16 +1,25 @@
 package me.yifeiyuan.onepiece.pandora.ktx
 
+import android.app.Activity
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.os.Build
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.annotation.RequiresApi
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
 /**
  * Created by 程序亦非猿 on 2021/4/7.
  */
+
+inline fun <reified A : Activity> Context.start(intentConfig: Intent.() -> Unit = {}) {
+    startActivity(Intent(this, A::class.java).apply(intentConfig))
+}
 
 fun Context.toPixel(dip: Int): Int {
     val scale: Float = resources.displayMetrics.scaledDensity
@@ -19,6 +28,10 @@ fun Context.toPixel(dip: Int): Int {
 
 fun Context.getScreenHeight(): Int {
     return resources.displayMetrics.heightPixels
+}
+
+fun Context.getScreenWidth(): Int {
+    return resources.displayMetrics.widthPixels
 }
 
 /**
@@ -55,13 +68,28 @@ fun Context?.copyTextToClipboard(text: CharSequence) {
     clipboardManager.setPrimaryClip(clip)
 }
 
+//inline fun <reified S>Context.getSystemService():S{
+//    return getSystemService(S::class.java)
+//}
+
 fun Context?.showSoftInput(view: View, flag: Int = InputMethodManager.SHOW_FORCED) {
     if (this == null) {
         return
     }
     val imm: InputMethodManager =
-            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 
     imm.showSoftInput(view, flag)
 }
 
+/**
+ * 判断网络是否连接
+ * @return 是否有网络可用
+ */
+fun Context.isNetworkAvailable(): Boolean {
+    val manager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    if (manager.activeNetworkInfo != null) {
+        return manager.activeNetworkInfo!!.isAvailable
+    }
+    return false
+}

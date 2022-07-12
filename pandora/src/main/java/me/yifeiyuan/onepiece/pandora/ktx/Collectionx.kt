aca1062c9021a37a948b4fa26e689e988aef31ad
mingjue <zhening.czn@alibaba-inc.com>
2022-07-12 11:05:20 +0800

    add typealias StringMap

--- pandora/src/main/java/me/yifeiyuan/onepiece/pandora/ktx/Collectionx.kt
+++ pandora/src/main/java/me/yifeiyuan/onepiece/pandora/ktx/Collectionx.kt
@@ -6,8 +6,16 @@ package me.yifeiyuan.onepiece.pandora.ktx
  * 集合相关 KTX
  */
 
+/**
+ * LinkedHashMap<String, String> 的类型别名
+ */
+typealias StringMap = LinkedHashMap<String, String>
 
-fun stringMap(init: (MutableMap<String, String>) -> Unit): MutableMap<String, String> {
+/**
+ * 快速构建一个 MutableMap<String, String>
+ * @return MutableMap<String, String>
+ */
+fun mapOfString(init: (MutableMap<String, String>) -> Unit): MutableMap<String, String> {
     return mutableMapOf<String, String>().apply {
         init.invoke(this)
     }

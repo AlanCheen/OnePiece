2cc310e56926bd12ea32de8888cb7266e9a45c68
Fitz <zhening.czn@alibaba-inc.com>
2022-07-26 14:56:58 +0800

    add Fragment.isActive()

--- pandora/src/main/java/me/yifeiyuan/onepiece/pandora/ktx/Fragmentx.kt
+++ pandora/src/main/java/me/yifeiyuan/onepiece/pandora/ktx/Fragmentx.kt
@@ -10,6 +10,17 @@ import androidx.fragment.app.Fragment
  * Created by 程序亦非猿 on 2021/4/12.
  */
 
+/**
+ *
+ * @return 是否是激活的状态
+ */
+fun Fragment.isActive(): Boolean {
+    if (activity == null) {
+        return false
+    }
+    return !activity!!.isFinishing && !activity!!.isDestroyed && isAdded
+}
+
 fun Fragment?.showToast(text: CharSequence?, duration: Int = Toast.LENGTH_SHORT) {
     if (this == null || text == null || this.activity == null) {
         return

caf0d748e62832e8d08e8f815784755d69acdfa1
Fitz <zhening.czn@alibaba-inc.com>
2022-09-28 11:31:55 +0800

    add func module

---
+++ func/src/main/java/me/yifeiyuan/onepiece/func/Blocks.kt
@@ -0,0 +1,27 @@
+package me.yifeiyuan.onepiece.func
+
+/**
+ * Created by 程序亦非猿 on 2022/9/28.
+ */
+
+typealias Block = () -> Unit
+
+typealias UnitBlock = () -> Unit
+
+typealias IntBlock = () -> Int
+
+typealias LongBlock = () -> Long
+
+typealias CharBlock = () -> Char
+
+typealias BooleanBlock = () -> Boolean
+
+typealias StringBlock = () -> String
+
+typealias NumberBlock = () -> Number
+
+typealias DoubleBlock = () -> Double
+
+typealias ValueBlock<T> = () -> T
+
+typealias NullableValueBlock<T> = () -> T?
---
+++ func/src/main/java/me/yifeiyuan/onepiece/func/Functional.kt
@@ -0,0 +1,5 @@
+package me.yifeiyuan.onepiece.func
+
+/**
+ * Created by 程序亦非猿 on 2022/9/28.
+ */

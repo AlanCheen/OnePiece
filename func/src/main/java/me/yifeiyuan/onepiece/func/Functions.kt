423150408d1af91029a16e6a9470d1413e2fef5e
Fitz <zhening.czn@alibaba-inc.com>
2022-12-03 00:18:22 +0800

    Rename again

---
+++ func/src/main/java/me/yifeiyuan/onepiece/func/Functions.kt
@@ -0,0 +1,28 @@
+package me.yifeiyuan.onepiece.func
+
+/**
+ * Created by 程序亦非猿 on 2022/9/28.
+ */
+
+typealias Func = () -> Unit
+
+typealias UnitFunc = () -> Unit
+
+typealias IntFunc = () -> Int
+
+typealias LongFunc = () -> Long
+
+typealias CharFunc = () -> Char
+
+typealias BooleanFunc = () -> Boolean
+
+typealias StringFunc = () -> String
+
+typealias NumberFunc = () -> Number
+
+typealias DoubleFunc = () -> Double
+
+typealias ValueFunc<R> = () -> R
+typealias ValueFunc1<P, R> = (P) -> R
+typealias ValueFunc2<P1, P2, R> = (P1, P2) -> R
+typealias ValueFunc3<P1, P2, P3, R> = (P1, P2, P3) -> R
